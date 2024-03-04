f013b4d61fb805020303e8cf69584f7cba301bb5
João Vitor Danielewicz <414732@unoesc.lan>
2024-03-04 20:21:41 -0300

    Comparação de Strings para valores booleanos

--- Exercicios/240304_01/Program.cs
+++ Exercicios/240304_01/Program.cs
@@ -1,1 +1,14 @@
-﻿// "Banana"
+﻿// Substituindo conteúdo de strings
+
+string nomeCompleto = "João Vitor Danielewicz";
+nomeCompleto = nomeCompleto.Replace("Vitor", "Paulo");
+
+Console.WriteLine(nomeCompleto);
+
+// Comparação de strings
+
+bool isNomeEqual = (nomeCompleto == "João Vitor Danielewicz");
+bool isNomeEqual2 = string.Equals(nomeCompleto, "João Paulo Danielewicz");
+
+Console.WriteLine($"Primeiro resultado: {isNomeEqual}");
+Console.WriteLine($"Segundo resultado: {isNomeEqual2}");
