d0ce77ef9c748f5637d94b517e41d6193861c741
João Vitor Danielewicz <414732@unoesc.lan>
2024-03-04 21:09:02 -0300

    Tipos numéricos

--- Exercicios/240304_01/Program.cs
+++ Exercicios/240304_01/Program.cs
@@ -12,3 +12,16 @@ bool isNomeEqual2 = string.Equals(nomeCompleto, "João Paulo Danielewicz");
 
 Console.WriteLine($"Primeiro resultado: {isNomeEqual}");
 Console.WriteLine($"Segundo resultado: {isNomeEqual2}");
+
+// Tipos Numéricos
+/*
+    sbyte: entre -128 e 127
+    short: entre -32.768 e 32.767
+    int: entre -2.147.483.648 e 2.147.483.647
+    long: entre - 9.223.372.036.854.775.808 e 9.223.372.036.854.775.807
+*/
+
+float myFloat = 10f;
+double myDouble = 5d;
+decimal myDecimal = 7m;
+int myInteger = 0;
