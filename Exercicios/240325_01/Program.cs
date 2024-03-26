79bd7d0920b831858d0b8335d95589edb39f4da5
João Vitor Danielewicz <414732@unoesc.lan>
2024-03-25 22:04:43 -0300

    Exemplificação de modificador Static

--- Exercicios/240325_01/Program.cs
+++ Exercicios/240325_01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _240325_01.Models;
+// Primeira clínica
+
+Clinica c1 = new Clinica();
+Clinica.InstanceCount = 1;
+c1.ObjectCount = 1;
+
+// Segunda clínica
+
+Clinica c2 = new Clinica();
+Clinica.InstanceCount++;
+c2.ObjectCount = 10;
+
+Console.WriteLine($"Valor c1: {c1.ObjectCount}\nInstance Count: {Clinica.InstanceCount}\nValor c2: {c2.ObjectCount}\n");
