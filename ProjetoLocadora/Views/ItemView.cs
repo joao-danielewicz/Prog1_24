93d2cb92bbec49d72ecc0ce6d18f621ab729ba4d
João Danielewicz <joao.wicz@gmail.com>
2024-06-18 08:50:50 -0300

    correção

---
+++ ProjetoLocadora/Views/ItemView.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoLocadora.Controllers;
+using ProjetoLocadora.Models;
+
+namespace ProjetoLocadora.Views
+{
+    public class ItemView{
+        
+    }
+}
